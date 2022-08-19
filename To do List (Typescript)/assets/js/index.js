9be1d81b04b8f8165f8dbb91fa37edfaabeca00a
Luccas Elias <luccaselias0@gmail.com>
2022-08-19 17:22:45 -0300

    Bug fix

--- To do List (Typescript)/assets/js/index.js
+++ To do List (Typescript)/assets/js/index.js
@@ -8,32 +8,37 @@ function createTaskArea() {
         alert('Name is required');
     }
     else {
-        totalTasks.push(taskName.value);
-        nextIndex = totalTasks.length;
-        var r = tableBody.insertRow();
-        var c1_1 = r.insertCell();
-        c1_1.innerHTML = taskName.value;
-        c1_1.classList.add("".concat(nextIndex - 1));
-        var c2 = r.insertCell();
-        c2.innerHTML = '<i class="bi bi-check"></i>';
-        c2.onclick = function () {
-            if (c1_1.style.textDecoration == 'none') {
-                c1_1.style.textDecoration = 'line-through';
-            }
-            else if (!c1_1.style.textDecoration) {
-                c1_1.style.textDecoration = 'line-through';
-            }
-            else {
-                c1_1.style.textDecoration = 'none';
-            }
-        };
-        var c3 = r.insertCell();
-        c3.innerHTML = '<i class="bi bi-x"></i>';
-        c3.onclick = function () {
-            var task = String(c1_1.textContent);
-            var rowID = totalTasks.indexOf(task);
-            tableBody.deleteRow(rowID);
-            totalTasks.splice(rowID, 1);
-        };
+        if (totalTasks.indexOf(taskName.value) != -1) {
+            alert('Task already added');
+        }
+        else {
+            totalTasks.push(taskName.value);
+            nextIndex = totalTasks.length;
+            var r = tableBody.insertRow();
+            var c1_1 = r.insertCell();
+            c1_1.innerHTML = taskName.value;
+            c1_1.classList.add("".concat(nextIndex - 1));
+            var c2 = r.insertCell();
+            c2.innerHTML = '<i class="bi bi-check"></i>';
+            c2.onclick = function () {
+                if (c1_1.style.textDecoration == 'none') {
+                    c1_1.style.textDecoration = 'line-through';
+                }
+                else if (!c1_1.style.textDecoration) {
+                    c1_1.style.textDecoration = 'line-through';
+                }
+                else {
+                    c1_1.style.textDecoration = 'none';
+                }
+            };
+            var c3 = r.insertCell();
+            c3.innerHTML = '<i class="bi bi-x"></i>';
+            c3.onclick = function () {
+                var task = String(c1_1.textContent);
+                var rowID = totalTasks.indexOf(task);
+                tableBody.deleteRow(rowID);
+                totalTasks.splice(rowID, 1);
+            };
+        }
     }
 }
--- To do List (Typescript)/assets/js/index.ts
+++ To do List (Typescript)/assets/js/index.ts
@@ -6,31 +6,36 @@ let nextIndex: number
 function createTaskArea(): void {
     if (taskName.value === '') {
         alert('Name is required')
-    } else {
-        totalTasks.push(taskName.value)
-        nextIndex = totalTasks.length
-        let r = tableBody.insertRow()
-        let c1 = r.insertCell()
-        c1.innerHTML = taskName.value
-        c1.classList.add(`${nextIndex-1}`)
-        let c2 = r.insertCell() as HTMLTableCellElement
-        c2.innerHTML = '<i class="bi bi-check"></i>'
-        c2.onclick = () => { 
-            if(c1.style.textDecoration == 'none'){
-                c1.style.textDecoration = 'line-through' 
-            }else if(!c1.style.textDecoration){
-                c1.style.textDecoration = 'line-through'
-            }else{
-                c1.style.textDecoration = 'none'
+    }
+    else {
+        if (totalTasks.indexOf(taskName.value) != -1) {
+            alert('Task already added')
+        } else {
+            totalTasks.push(taskName.value)
+            nextIndex = totalTasks.length
+            let r = tableBody.insertRow()
+            let c1 = r.insertCell()
+            c1.innerHTML = taskName.value
+            c1.classList.add(`${nextIndex - 1}`)
+            let c2 = r.insertCell() as HTMLTableCellElement
+            c2.innerHTML = '<i class="bi bi-check"></i>'
+            c2.onclick = () => {
+                if (c1.style.textDecoration == 'none') {
+                    c1.style.textDecoration = 'line-through'
+                } else if (!c1.style.textDecoration) {
+                    c1.style.textDecoration = 'line-through'
+                } else {
+                    c1.style.textDecoration = 'none'
+                }
             }
-        }
-        let c3 = r.insertCell() as HTMLTableCellElement
-        c3.innerHTML = '<i class="bi bi-x"></i>'
-        c3.onclick = () => {
-            let task = String(c1.textContent)
-            let rowID = totalTasks.indexOf(task)
-            tableBody.deleteRow(rowID)
-            totalTasks.splice(rowID,1)
+            let c3 = r.insertCell() as HTMLTableCellElement
+            c3.innerHTML = '<i class="bi bi-x"></i>'
+            c3.onclick = () => {
+                let task = String(c1.textContent)
+                let rowID = totalTasks.indexOf(task)
+                tableBody.deleteRow(rowID)
+                totalTasks.splice(rowID, 1)
+            }
         }
     }
 }
